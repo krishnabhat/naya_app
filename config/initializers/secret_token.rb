9e1c37d73fab4c8d02b13e38015973b8ad003bea
Krishna Bhat <krishna.bhat@gmail.com>
2014-06-08 15:53:03 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NayaApp::Application.config.secret_token = '49b8e01ceb48b85e9ff7e25fa20f940effa08f8de34bb90d79c1207adea6f4f85490fb67b1b0ee7b1fe345b861ebf86af8a0e2f6899ceae879661c7140ea89f2'
